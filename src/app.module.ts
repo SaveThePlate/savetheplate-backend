fa4f5ae07ec817cc46d0e821b50208fbfe863ae0
LaifiCode <146626955+LaifiCode@users.noreply.github.com>
2024-08-23 09:56:21 +0100

    Implement product API with Prisma client service (#3)
    Co-authored-by: Sarra Sou <52163754+sarrasoussia@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { OfferModule } from './offer/offer.module';
-import { StorageModule } from './storage/storage.module';
+import { StorageModule } from './storage/storage.module';import { ProductsModule } from './products/products.module';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
-  imports: [AuthModule, OfferModule, StorageModule],
-
+  imports: [ ProductsModule,AuthModule, OfferModule, StorageModule],
   controllers: [AppController],
-  providers: [AppService,
-                ],
+  providers: [AppService,PrismaService],
+  exports: [PrismaService],
 })
-export class AppModule {};
+export class AppModule {}
+
+
+
