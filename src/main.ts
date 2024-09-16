1051f30db4389cf1b45e7112d57f75745b8c3ff6
Karim <4542559+hacKarim@users.noreply.github.com>
2024-09-16 17:11:43 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'));
   
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   });
