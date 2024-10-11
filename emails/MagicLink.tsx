91fc7f70269e55a3fc6b76463594cd23fd721571
Sarra <sarrasoussia2001@gmail.com>
2024-10-11 21:01:08 +0100

    edited the magic link email

--- emails/MagicLink.tsx
+++ emails/MagicLink.tsx
@@ -3,19 +3,7 @@ import MainTemplate from './MainTemplate';
 
 function MagicLinkEmailTemplate({ magicLink }) {
   return (
-    <MainTemplate
-      preview="Connect to Save The Plate"
-      mainTitle="Happy to see you, again!"
-      //image bug here
-      mainImage={`${process.env.FRONT_URL}/fullname1.png`}
-      description="Just click the button below to log in."
-      centeredDescription={true}
-      details={[]}
-      buttonText="Login"
-      buttonLink={magicLink}
-      withThank={true}
-      withButton={true}
-    />
+    <MainTemplate buttonLink={magicLink} />
   );
 }
 
